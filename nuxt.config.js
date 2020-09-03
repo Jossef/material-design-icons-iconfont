3f661afd3bcff9a0e741f7734bf2e5e4318d752b
Jossef Harush <jossef12@gmail.com>
2020-09-03 08:03:38 +0000

    docs(): refactoring build scripts + adding docs in main branch
    #56

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  router: {
+    base: '/material-design-icons-iconfont/'
+  },
+  dir: {
+    assets: 'docs/assets',
+    app: 'docs/app',
+    layouts: 'docs/layouts',
+    middleware: 'docs/middleware',
+    pages: 'docs/pages',
+    static: 'docs/static',
+    store: 'docs/store'
+  },
+  generate: {
+    dir: 'dist/docs'
+  }
+}
